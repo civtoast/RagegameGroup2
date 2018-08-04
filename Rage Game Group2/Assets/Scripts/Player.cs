de4c44f6629f6acba1936de7783f511f8ee74042
Neil van der merwe <necktro101@gmail.com>
2018-08-04 14:32:34 +0200

    add things

---
+++ Rage Game Group2/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 180f;
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+        Vector3 movement = new Vector3(0f, 0f, vertical);
+        Vector3 rotation = new Vector3(0f, horizontal, 0f);
+
+        transform.Translate(movement * Time.deltaTime * moveSpeed);
+        transform.Rotate(rotation * Time.deltaTime * rotationSpeed);
+
+
+
+
+    }
+}
+
+   
